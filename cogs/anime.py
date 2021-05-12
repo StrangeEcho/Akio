df038d2299d982a169ec49ccb6d5b6f228c9bd6f
Yato <tylergoodmen312@gmail.com>
2021-05-12 11:07:25 -0400

    Make Epic Ass AnimeQuote Commands

--- cogs/anime.py
+++ cogs/anime.py
@@ -32,6 +32,18 @@ class Waifus(commands.Cog):
                 embed = discord.Embed(color=0x33DAFF)
                 embed.set_image(url=shinobu)
                 await ctx.send(embed=embed)
+    
+    @commands.command()
+    @commands.cooldown(1, 5, commands.BucketType.user) # ratelimit is 100 per 1 hour so rip 
+    async def animequote(self, ctx: commands.Context):
+        """Get a sick ass anime quote"""
+        async with aiohttp.ClientSession() as cs:
+            async with cs.get("https://animechan.vercel.app/api/random") as resp:
+                #vars
+                char = (await resp.json())["character"]
+                quote = (await resp.json())["quote"]
+                anime = (await resp.json())["anime"]
+                await ctx.send(embed=discord.Embed(description=f"{quote}\n~{char}", color=0xFFB6C1).set_footer(text=f"Quote From: {anime}"))
 
 def setup(bot):
     bot.add_cog(Waifus(bot))
